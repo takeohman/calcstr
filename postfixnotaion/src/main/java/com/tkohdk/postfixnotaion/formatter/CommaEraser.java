0703a3eaa1d22eb2eccdbadb1b92004355bc0c9e
Takeo Hidaka <takeohman@gmail.com>
2018-12-19 01:46:14 +0900

    * PostfixNotationUtil class is deleted. * Remove the classes for formatting numbers.

--- postfixnotaion/src/main/java/com/tkohdk/postfixnotaion/formatter/CommaEraser.java
+++
@@ -1,8 +0,0 @@
-package com.tkohdk.postfixnotaion.formatter;
-
-public class CommaEraser extends AbstractFormatter{
-    @Override
-    protected String myFormat(String val) {
-        return val.replace(",","");
-    }
-}
